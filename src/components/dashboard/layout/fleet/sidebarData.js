73fabb3c972f8652a369b4712a6c4142af29d743
Mohamed Ashraf <mohamed.dev.egy@gmail.com>
2024-02-24 15:54:22 +0200

    edited add yacht router

--- src/components/dashboard/layout/fleet/sidebarData.js
+++ src/components/dashboard/layout/fleet/sidebarData.js
@@ -1,46 +1,46 @@
 const sidebarData = [
   {
-    path: "main-info",
+    path: "",
     icon: require("../../../../assets/images/info.svg").default,
     alt: "main-info",
-    label: "Main Info"
+    label: "Main Info",
   },
   {
     path: "media-photos",
     icon: require("../../../../assets/images/media.svg").default,
     alt: "media-photos",
-    label: "Media & Photos"
+    label: "Media & Photos",
   },
   {
     path: "boat-specification",
     icon: require("../../../../assets/images/ancor.svg").default,
     alt: "boat-specification",
-    label: "Boat Specification"
+    label: "Boat Specification",
   },
   {
     path: "working-hours",
     icon: require("../../../../assets/images/timer.svg").default,
     alt: "working-hours",
-    label: "Working Hours"
+    label: "Working Hours",
   },
   {
     path: "pricing",
     icon: require("../../../../assets/images/wallet.svg").default,
     alt: "pricing",
-    label: "Pricing"
+    label: "Pricing",
   },
   {
     path: "add-ons-connected",
     icon: require("../../../../assets/images/AddOns.svg").default,
     alt: "add-ons-connected",
-    label: "Add Ons Connected"
+    label: "Add Ons Connected",
   },
   {
     path: "more-info",
     icon: require("../../../../assets/images/more.svg").default,
     alt: "more-info",
-    label: "More Info"
-  }
+    label: "More Info",
+  },
 ];
 
 export default sidebarData;
