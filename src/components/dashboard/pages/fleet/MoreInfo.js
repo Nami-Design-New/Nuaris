dc004e62e94402d4f498b53aaa3604b844fc6326
Mohamed Ashraf <mohamed.dev.egy@gmail.com>
2024-03-05 21:41:42 +0200

    addons

--- src/components/dashboard/pages/fleet/MoreInfo.js
+++ src/components/dashboard/pages/fleet/MoreInfo.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
 
 const MoreInfo = () => {
   return (
-    <div>MoreInfo</div>
-  )
-}
+    <div className="fleet_form__wrapper">
+      <div className="bg_white_card">
+        <form className="form-ui">
+          <div className="row m-0">
+            <div className="col-12 p-2">
+              <h6 className="form_title">Amenities & Inclusions</h6>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className="bg_white_card">
+        <form className="form-ui">
+          <div className="row m-0">
+            <div className="col-12 p-2">
+              <h6 className="form_title">Navigation and Safety</h6>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className="bg_white_card">
+        <form className="form-ui">
+          <div className="row m-0">
+            <div className="col-12 p-2">
+              <h6 className="form_title">Salons and Cabins</h6>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className="bg_white_card">
+        <form className="form-ui">
+          <div className="row m-0">
+            <div className="col-12 p-2">
+              <h6 className="form_title">Entertainment</h6>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className="bg_white_card">
+        <form className="form-ui">
+          <div className="row m-0">
+            <div className="col-12 p-2">
+              <h6 className="form_title">Complimentary Inclusions</h6>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className="bg_white_card">
+        <form className="form-ui">
+          <div className="row m-0">
+            <div className="col-12 p-2">
+              <h6 className="form_title">Connectivity</h6>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default MoreInfo
+export default MoreInfo;
