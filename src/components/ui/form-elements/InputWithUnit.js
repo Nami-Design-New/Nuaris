2d1628c1e9f5bb36e14e41bcf0e9a7ece89554dd
Ahmedelsayed265 <https://github.com/Ahmedelsayed265>
2024-03-13 14:10:43 +0200

    #policy form correction start addon design

--- src/components/ui/form-elements/InputWithUnit.js
+++ src/components/ui/form-elements/InputWithUnit.js
@@ -6,7 +6,9 @@ const InputWithUnit = ({
   units,
   hint,
   formData,
-  setFormData
+  setFormData,
+  idx,
+  innerTarget,
 }) => {
   const [unit, setUnit] = useState(units[0]);
   const [value, setValue] = useState("");
@@ -23,7 +25,16 @@ const InputWithUnit = ({
 
   const handleChange = (newValue, newUnit) => {
     const combined = `${newValue} ${newUnit}`;
-    setFormData({ ...formData, [htmlFor]: combined });
+    if (!innerTarget) {
+      setFormData({ ...formData, [htmlFor]: combined });
+    } else {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [innerTarget]: prevFormData[innerTarget].map((item, i) =>
+          i === idx ? { ...item, [htmlFor]: combined } : item
+        ),
+      }));
+    }
   };
 
   return (
