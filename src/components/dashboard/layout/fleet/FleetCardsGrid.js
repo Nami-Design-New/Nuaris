bb67780e3a103a4e0956cd368e7ec58338b8bccb
Mohamed Ashraf <mohamed.dev.egy@gmail.com>
2024-03-20 17:07:59 +0200

    pagination structure

--- src/components/dashboard/layout/fleet/FleetCardsGrid.js
+++ src/components/dashboard/layout/fleet/FleetCardsGrid.js
@@ -5,7 +5,7 @@ export default function FleetCardsGrid() {
   const yachts = useSelector((state) => state.yachts.yachts);
   return (
     <div className="row m-0">
-      {yachts.map((yacht) => (
+      {yachts?.results?.map((yacht) => (
         <div className="col-lg-3 col-md-6 col-12 p-2" key={yacht.id}>
           <FleetCard fleet={yacht} />
         </div>
