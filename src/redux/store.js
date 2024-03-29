6294feb681e3b4bed6388384368fa2a76ad5f728
Ahmedelsayed265 <https://github.com/Ahmedelsayed265>
2024-03-29 15:23:34 +0200

    assign group pre handle

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import user from "./slices/authenticatedUserSlice";
-import users from "./slices/subSetUsers";
 import positions from "./slices/positions";
-import employees from "./slices/employeesSlice";
-import permissionsGroups from "./slices/permissionsGroups";
-import permissions from "./slices/permissions";
-import yachts from "./slices/yachts";
 
 export const store = configureStore({
   reducer: {
     user,
-    users,
-    positions,
-    employees,
-    permissionsGroups,
-    permissions,
-    yachts,
-  },
+    positions
+  }
 });
