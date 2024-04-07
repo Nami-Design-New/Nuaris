50164cb4877021993c81d3ec38117fda26761ffe
Ahmedelsayed265 <https://github.com/Ahmedelsayed265>
2024-04-07 09:51:15 +0200

    pricing

--- src/components/dashboard/layout/packages/PackagePriceTime.js
+++ src/components/dashboard/layout/packages/PackagePriceTime.js
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 import { DAYS } from "../../../../constants";
 import PricingAccordion from "./PricingAccordion";
 import SubmitButton from "../../../ui/form-elements/SubmitButton";
+import axios from "./../../../../util/axios";
+import { toast } from "react-toastify";
 
 const PackagePriceTime = ({ setForm }) => {
+  const [loading, setLoading] = useState(false);
+  const packageId = sessionStorage.getItem("package_id");
   const periodInitial = {
     start_date: "",
     end_date: "",
     price: "",
-    price_type: ""
+    price_type: "per person"
   };
   const formDataInitial = DAYS.map((day, index) => {
     return {
@@ -19,15 +23,47 @@ const PackagePriceTime = ({ setForm }) => {
     };
   });
   const [formData, setFormData] = useState(formDataInitial);
-  const [loading] = useState(false);
 
   const handleBack = (e) => {
     e.preventDefault();
     setForm("Package Info");
   };
 
+  const handleSubmit = async (e) => {
+    console.log(formData);
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const filteredFormData = formData.filter((obj) => obj.selected === true);
+      const reqData = filteredFormData.map((obj) => {
+        return {
+          day: obj.day,
+          periods: obj.periods
+        };
+      });
+      const dictionary = {
+        trip_package_days: reqData
+      };
+      const response = await axios.patch(
+        `/trip-packages/${packageId}/`,
+        dictionary
+      );
+      if (response.status === 200) {
+        toast.success("Package Time & Price Saved Successfully");
+        setForm("Policy");
+      } else {
+        toast.error("Something went wrong");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <form className="form-ui">
+    <form className="form-ui" onSubmit={handleSubmit}>
       <div className="row">
         <div className="col-12 p-2">
           <h6 className="form_title">Package Time & Price</h6>
--- src/components/dashboard/layout/packages/PricePeriodRow.js
+++ src/components/dashboard/layout/packages/PricePeriodRow.js
@@ -1,41 +1,100 @@
 import React from "react";
+import deleteIcon from "../../../../assets/images/delete.svg";
 import addIcon from "../../../../assets/images/addRow.svg";
 import CustomInputField from "./../../../ui/form-elements/CustomInputField";
 import CustomInputWithUnit from "./../../../ui/form-elements/CustomInputWIthUnit";
 
-const PricePeriodRow = () => {
+const PricePeriodRow = ({
+  day,
+  index,
+  currentObject,
+  setFormData,
+  formData
+}) => {
+  const handleAddRow = () => {
+    currentObject.periods.push({
+      start_date: "",
+      end_date: "",
+      price: "",
+      price_type: ""
+    });
+    setFormData((prev) => [
+      ...prev.filter((obj) => obj.day !== day),
+      currentObject
+    ]);
+  };
+
+  const handleDeleteRow = () => {
+    currentObject.periods.splice(index, 1);
+    setFormData((prev) => [
+      ...prev.filter((obj) => obj.day !== day),
+      currentObject
+    ]);
+  };
+
+  function handleChange(value, key, index) {
+    currentObject.periods[index][key] = value;
+    setFormData((prev) => {
+      const currentIndex = prev.findIndex((obj) => obj.day === day);
+      const newFormData = [...prev];
+      newFormData[currentIndex] = currentObject;
+      return newFormData;
+    });
+  }
+
   return (
     <div className="price_period_row">
       <div className="row m-0">
         <div className="col-12 p-2">
           <div className="title">
             <h6>Package Time & Price</h6>
-            <button>
-              <img src={addIcon} alt="add" />
-            </button>
+            {index === 0 ? (
+              <button
+                type="button"
+                disabled={currentObject.periods.length >= 3}
+                onClick={handleAddRow}
+              >
+                <img src={addIcon} alt="add icon" />
+              </button>
+            ) : (
+              <button type="button" onClick={handleDeleteRow}>
+                <img src={deleteIcon} alt="delete icon" />
+              </button>
+            )}
           </div>
         </div>
         <div className="col-lg-6 col-12 p-2">
           <CustomInputField
-            label="Start Date"
-            type="date"
+            label="Start time"
+            type="datetime-local"
             id="start_date"
             name="start_date"
+            required
+            onChange={(e) => handleChange(e.target.value, "start_date", index)}
           />
         </div>
         <div className="col-lg-6 col-12 p-2">
           <CustomInputField
-            label="End Date"
-            type="date"
+            label="End time"
+            type="datetime-local"
             id="end_date"
             name="end_date"
+            required
+            onChange={(e) => handleChange(e.target.value, "end_date", index)}
           />
         </div>
         <div className="col-12 p-2">
           <CustomInputWithUnit
             label="Price"
             name={"price"}
+            value={formData[index].price}
+            selectValue={formData[index].price_type}
             units={["per person", "per trip"]}
+            onChange={(e) => handleChange(e.target.value, "price", index)}
+            selectName="price_type"
+            selectOnChange={(e) =>
+              handleChange(e.target.value, "price_type", index)
+            }
           />
         </div>
       </div>
