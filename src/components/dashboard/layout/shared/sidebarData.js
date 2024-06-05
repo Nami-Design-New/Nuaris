4b710c771d61fc004160c27184679a22f4729422
Ahmed Abd El-Ghany <ahmed.abdelghany1211@gmail.com>
2024-06-05 04:15:10 +0300

    updating asidebar

--- src/components/dashboard/layout/shared/sidebarData.js
+++ src/components/dashboard/layout/shared/sidebarData.js
@@ -7,81 +7,97 @@ const sidebarData = [
   },
   {
     path: "fleet",
-    icon: require("../../../../assets/images/Fleet.svg").default,
-    alt: "fleet-icon",
-    label: "Fleet",
+    icon: require("../../../../assets/images/manage-listing.svg").default,
+    alt: "Manage-Listings-icon",
+    label: "Manage Listings",
+    submenu: [
+      {
+        path: "fleet",
+        label: "Fleet",
+      },
+      {
+        path: "addons",
+        label: "Addons",
+      },
+      {
+        path: "activities",
+        label: "Activities",
+      },
+      {
+        path: "trip-packages",
+        label: "Trip Packages",
+      },
+      {
+        path: "booking",
+        label: "Booking",
+      },
+      {
+        path: "destination",
+        label: "Destination",
+      },
+      {
+        path: "packages",
+        label: "Packages",
+      },
+    ],
   },
   {
-    path: "addons",
-    icon: require("../../../../assets/images/AddOns.svg").default,
-    alt: "add-ones-icon",
-    label: "Addons",
-  },
-  {
-    path: "activities",
-    icon: require("../../../../assets/images/activities.svg").default,
-    alt: "trip-packages-icon",
-    label: "Activities",
-  },
-  {
-    path: "trip-packages",
-    icon: require("../../../../assets/images/TripPackages.svg").default,
-    alt: "trip-packages-icon",
-    label: "Trip Packages",
-  },
-  {
-    path: "destination",
-    icon: require("../../../../assets/images/Destination.svg").default,
-    alt: "destination-icon",
-    label: "Destination",
-  },
-  {
-    path: "affiliate",
-    icon: require("../../../../assets/images/Affiliate.svg").default,
-    alt: "affiliate-icon",
-    label: "Affiliate",
-  },
-  {
-    path: "clients",
-    icon: require("../../../../assets/images/Clients.svg").default,
-    alt: "client-icon",
-    label: "Clients",
+    path: "agent-request",
+    icon: require("../../../../assets/images/markiting-promotion.svg").default,
+    alt: "Marketing-Promotion-icon",
+    label: "Markiting & Promotions",
+    submenu: [
+      {
+        path: "agent-request",
+        label: "Agent Requests",
+      },
+      {
+        path: "affiliate",
+        label: "Affiliate",
+      },
+      {
+        path: "compignes",
+        label: "Compignes",
+      }
+    ],
   },
   {
     path: "reports",
     icon: require("../../../../assets/images/Reports.svg").default,
-    alt: "reports-icon",
-    label: "Reports",
+    alt: "analytics-icon",
+    label: "Analytics",
+    submenu: [{
+      path: "reports",
+      label: "Reports",
+    }, {
+      path: "clients",
+      label: "Clients",
+    }]
   },
+  
   {
-    path: "packages",
-    icon: require("../../../../assets/images/packages.svg").default,
+    path: "guests",
+    icon: require("../../../../assets/images/guest-details.svg").default,
     alt: "packages-icon",
-    label: "Packages",
-  },
-  {
-    path: "complaint",
-    icon: require("../../../../assets/images/complaint.svg").default,
-    alt: "complaint-icon",
-    label: "Complaint",
-  },
-  {
-    path: "rating",
-    icon: require("../../../../assets/images/Rating.svg").default,
-    alt: "rating-icon",
-    label: "Rating",
-  },
-  {
-    path: "host-settings",
-    icon: require("../../../../assets/images/Hostsettings.svg").default,
-    alt: "host-settings-icon",
-    label: "Host settings",
-  },
-  {
-    path: "questions-settings",
-    icon: require("../../../../assets/images/QuestionsSettings.svg").default,
-    alt: "questions-settings-icon",
-    label: "Questions Settings",
+    label: "Guest Details &  Reviews",
+    submenu: [
+      {
+        path: "guests",
+        label: "Guests",
+      },
+      {
+        path: "complaint",
+        label: "Complaint",
+      },
+      {
+        path: "rating",
+        label: "Rating",
+      },
+      {
+        path: "questions-settings",
+        label: "Questions",
+      },
+    ]
   },
 ];
 
