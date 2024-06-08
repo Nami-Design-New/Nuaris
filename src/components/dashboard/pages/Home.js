906fb92a324f24f43d401bd52b2d31bddb8d4fad
Ahmed Abd El-Ghany <ahmed.abdelghany1211@gmail.com>
2024-06-09 01:48:43 +0300

    Finishing the dashboard pdage

--- src/components/dashboard/pages/Home.js
+++ src/components/dashboard/pages/Home.js
@@ -4,6 +4,8 @@ import SchedulingCalender from "../layout/scheduling/SchedulingCalender";
 import Bookings from "../layout/scheduling/Bookings";
 import Weather from "../layout/Home/Weather";
 import BookingsStats from "../layout/Home/BookingsStats";
+import SupplyDemand from "../layout/Home/SupplyDemand";
+import DashboardStats from "../layout/Home/DashboardStats";
 
 const Home = () => {
   return (
@@ -20,6 +22,12 @@ const Home = () => {
         </div>
         <div className="col-12 p-2">
           <BookingsStats />
+        </div>
+        <div className="col-12 p-2">
+          <SupplyDemand />
+        </div>
+        <div className="col-12 p-2">
+          <DashboardStats />
         </div>
         <div className="col-12 p-2">
           <Bookings />
